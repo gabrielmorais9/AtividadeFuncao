7438c51b65ecd484797b644272e80c311066ac04
Gabriel <gabrielmoraisreis2@gmail.com>
2026-09-22 16:25:45 -0300

    Add files via upload

---
+++ 1/js.js
@@ -0,0 +1,20 @@
+/*1. Crie uma função chamada calcularAreaRetangulo que receba dois parâmetros:
+base e altura. A função deve calcular e retornar o valor da área (área = base ×
+altura).*/
+
+function calc(n1,n2) {
+    let resultado = n1 * n2
+    return resultado
+}
+function receb(){
+    a = Number(prompt("Digite a altura: "))
+    b = Number(prompt("Digite a base: "))
+}
+function imprim(valor){
+    alert(`O valor da area desse retangulo é ${valor}`)
+}
+let a , b
+
+receb()
+let result = calc(a,b)
+imprim(result)
